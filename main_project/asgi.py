2c4a789bc9b02dde1ce7d0871225d6586755ac8c
clydeeag <clydeeag@student.21-school.ru>
2024-12-15 21:46:01 +0300

    created model Product

--- main_project/asgi.py
+++ main_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for main_project project.
+ASGI config for main_project main_project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- main_project/wsgi.py
+++ main_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for main_project project.
+WSGI config for main_project main_project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
